52924f45e9127594be1bd25afe05a324840c525d
bparekh2 <bparekh2@gmail.com>
2021-05-06 11:49:26 -0400

    React And django working and displaying data admin side

--- todocrud/settings.py
+++ todocrud/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig',  # added
+    'todos.apps.TodosConfig',  # added
+    'rest_framework',  # added
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
